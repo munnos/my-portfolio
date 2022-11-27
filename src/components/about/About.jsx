cb99942e5227c11e75a10a9859ee43bd53167bef
Munnos <munnos@hotmail.co.uk>
2022-11-27 18:21:40 +0000

    change to styling of about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,36 +10,27 @@ const About = () => {
       
 
           <div className="aboutContent">
-          <div className="aboutCards">
-          <article className="aboutCard">
+          <article className="aboutCards">
             <h5> Strong legal professional seeking first role in web development</h5>
             
           </article>
           </div>
 
           <div className="aboutContent">
-          <div className="aboutCards">
-          <article className="aboutCard">
+          <article className="aboutCards">
             <h5> Ipsom doriefeafa</h5>
           </article>
           </div>
 
           <div className="aboutContent">
-          <div className="aboutCards">
-          <article className="aboutCard">
-            <h5> Ipsom doriefeafa</h5>
-          </article>
+          <article className="aboutCards">
+            <a href="/contact" className='btnPrimary'> Contact Me! </a>
+            </article>
           </div>
-          </div>
-
-          <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Quod qui ipsa dolorem ab nemo vel corrupti. Dolores itaque libero expedita. Animi quos libero nesciunt obcaecati laudantium. Autem deserunt aut unde.</p>
-
-            <a href="#contact" className='btnPrimary'> Contact Me! </a>
-
         </div>
-        </div>
-        </div>
+        
+        
+  
 
 
 
